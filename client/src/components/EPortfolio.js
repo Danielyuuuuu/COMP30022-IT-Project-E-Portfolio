6b0798db17b748b69ac691b436b9c7c9db749e54
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-09-05 00:56:32 +1000

    add NavbarTop component

--- client/src/components/EPortfolio.js
+++ client/src/components/EPortfolio.js
@@ -6,7 +6,7 @@ class EPortfolio extends Component {
     render(){
         return(
             <div>
-                <h1>This is an EPortfolio</h1>
+                <NavbarTop />
                 
             </div>
         );
