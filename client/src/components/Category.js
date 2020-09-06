e80a98ae8fb7a8fabfd3826651a59b5d99cd5335
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-09-07 04:07:49 +1000

    changed category displayed pictures

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -12,15 +12,16 @@ const Example = (props) => {
     <div>
     <Row>
       <Col sm="4">
-        <Card body>
-            <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
-            <CardFooter className="text-center">Graphic Design</CardFooter>
-        </Card>
+      <Card body>
+      <CardImg width="100%" src="https://static.dezeen.com/uploads/2019/12/logo-house-adidas-karina-wiciak_dezeen_2364_col_2.jpg" alt="Card image cap" />
+      <CardFooter className="text-center">Architecture</CardFooter>
+      </Card>
+        
       </Col>
 
       <Col sm="4">
         <Card body>
-        <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
+        <CardImg width="80%" src="https://www.business.com/images/content/5ca/3d01e5a215e8a458b690f/0-800-" alt="Card image cap" />
         <CardFooter className="text-center">Photography</CardFooter>
         </Card>
       </Col>
@@ -39,23 +40,23 @@ const Example = (props) => {
     <Row>
     <Col sm="4">
       <Card body>
-          <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
+          <CardImg width="100%" src="https://wallpaperaccess.com/full/764272.jpg" alt="Card image cap" />
           <CardFooter className="text-center">Painting</CardFooter>
       </Card>
     </Col>
 
     <Col sm="4">
       <Card body>
-      <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
+      <CardImg width="100%" src="https://sofy.tv/blog/wp-content/uploads/2020/01/filmmaking-production.jpg" alt="Card image cap" />
       <CardFooter className="text-center">Films</CardFooter>
       </Card>
     </Col>
 
     <Col sm="4">
-      <Card body>
-      <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
-      <CardFooter className="text-center">Architecture</CardFooter>
-      </Card>
+    <Card body>
+            <CardImg width="100%" src="https://www.thecreativeissue.com.au/wp-content/uploads/2017/07/Beastman-3-TCI.jpg" alt="Card image cap" />
+            <CardFooter className="text-center">Graphic Design</CardFooter>
+        </Card>
     </Col>
 
     </Row>
