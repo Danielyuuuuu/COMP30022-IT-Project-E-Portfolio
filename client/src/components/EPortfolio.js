522c60f8c3badadc3d9c11ec7ed1e1b27cba2aa9
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-09-07 03:32:15 +1000

    add HomeSlides

--- client/src/components/EPortfolio.js
+++ client/src/components/EPortfolio.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
 import NavbarTop from "./NavbarTop";
 import Category from "./Category";
+import HomeSlide from "./HomeSlide";
 import "../App.css";
+
 
 class EPortfolio extends Component {
     render(){
         return(
             <div>
                 <NavbarTop/>
-                <Category />
+                <HomeSlide />
                 <br />
                 <br />
-                <Category />
-                <br />
-                <br />
-                <Category />
+                
                 
             </div>
         );
---
+++ client/src/components/HomeSlide.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
+
+const items = [
+  {
+    src: 'https://i.pinimg.com/originals/db/1c/1a/db1c1ad07b38e8feab26ab31c2961b75.jpg',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    header: 'Product Design',
+    key: '1'
+  },
+  {
+    src: 'https://i.redd.it/wc2vtyzlnmw41.jpg',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    header: 'Photography & Imaging',
+    key: '2'
+  },
+  {
+    src: 'http://sf.co.ua/16/08/wallpaper-3b352.jpg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'Graphic Design',
+    key: '3'
+  }
+];
+
+const HomeSlide = () => <UncontrolledCarousel items={items} />;
+
+export default HomeSlide;
