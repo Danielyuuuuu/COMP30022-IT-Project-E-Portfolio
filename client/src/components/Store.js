276b67b057cffbf13732bb5867c15c8812eb33a9
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-09-07 01:20:39 +1000

    add NavbarTop to the Store component

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import NavbarTop from "./NavbarTop";
-import Category from "./Category";
 import "../App.css";
 
 class Store extends Component{
     render(){
         return(
-            <h1>This is the store.</h1>
+            <div>
+                <NavbarTop />
+                <h1>This is the store.</h1>
+            </div>
         );
     }
 }
