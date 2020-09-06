445d7e306612a4a3de2d90423c3434b7848196a9
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-09-07 03:28:44 +1000

    add items into store

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -1,6 +1,43 @@
+
 import React, { Component } from "react";
 import NavbarTop from "./NavbarTop";
 import "../App.css";
+
+
+
+import { Card, Button, CardTitle, CardText, CardImg, Row, Col } from 'reactstrap';
+
+const Example = (props) => {
+  return (
+    <Row>
+      <Col sm="3">
+        <Card body>
+            <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
+          <CardTitle>Art product  $5.00</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Buy now</Button>
+        </Card>
+      </Col>
+
+      <Col sm="3">
+        <Card body>
+        <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
+          <CardTitle>Art product  $5.00</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Buy now</Button>
+        </Card>
+        
+      </Col>
+    </Row>
+
+        
+
+  );
+};
+
+const divStyle = {
+  };
+
 
 class Store extends Component{
     render(){
@@ -10,7 +47,9 @@ class Store extends Component{
                 <br />
                 <br />
                 <br />
-                <h5>This is the store.</h5>
+                <div style={divStyle}>
+                    <Example />
+                </div>
             </div>
         );
     }
