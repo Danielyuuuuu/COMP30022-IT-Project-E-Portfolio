91383b6ebbbebd2fdd4066e5dbcba3bd2b29cc9e
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-09-08 00:21:32 +1000

    edited Store's images

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -27,19 +27,19 @@ const Example = (props) => {
         </Card>
 
         <Card body>
-            <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
-          <CardTitle>Art product  $5.00</CardTitle>
+            <CardImg width="100%" src="https://www.montmarte.net/assets/Uploads/_resampled/FillWyI5MDAiLCI1MDAiXQ/Leonid-Afremov-inspired-painting-1.jpg" alt="Card image cap" />
+          <CardTitle>Painting  $10.00</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
           <Button>Buy now</Button>
         </Card>
 
         <Card body>
-        <CardImg width="100%" src="https://www.fineartbyhelenreimer.com/images/intercessory-worship-high-res.jpg" alt="Card image cap" />
-          <CardTitle>Painting  $7.00</CardTitle>
+        <CardImg width="100%" src="https://redballoon.com.au/dw/image/v2/BCRD_PRD/on/demandware.static/-/Sites-rb-au-catalog/default/images/products/PWA014-M/hhaubjsf4lbthjnsmfjs.jpg?sw=784&sh=431&q=70" alt="Card image cap" />
+          <CardTitle>Photography  $3.00</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
           <Button>Buy now</Button>
         </Card>
-        
+
     </div> 
 
         
