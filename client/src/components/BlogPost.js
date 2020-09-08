9eaf86f54faf4670552cfa88b8fc839bcb225d85
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-09 00:46:58 +1000

    Changed blog posts position

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -21,25 +21,34 @@ class BlogPost extends Component {
           </div>
           <div className="blogText">
             <h1 className="blogTitle">Blog Title</h1>
-            <h2>Blog Text</h2>
+            <h2 className="blogBody">Blog Text</h2>
           </div>
         </div>
         <div className="blogPost">
           <div className="postImage">
-            <img src="https://petapixel.com/assets/uploads/2019/11/Geert-Weggen_squirrel-wishes_00003677.jpg" />
+            <img src="https://filmdaily.co/wp-content/uploads/2020/04/funny-animals-lede.jpg" />
           </div>
           <div className="blogText">
             <h1 className="blogTitle">Blog Title</h1>
-            <h2>Blog Text</h2>
+            <h2 className="blogBody">Blog Text</h2>
           </div>
         </div>
         <div className="blogPost">
           <div className="postImage">
-            <img src="https://petapixel.com/assets/uploads/2019/11/Geert-Weggen_squirrel-wishes_00003677.jpg" />
+            <img src="https://www.nikon.com.au/tmp/Asia/2419865273/3760176746/2586568015/286546384/152549275/210978916/542011102/2974987658/1834978357/200150890/2821987493.jpg" />
           </div>
           <div className="blogText">
             <h1 className="blogTitle">Blog Title</h1>
-            <h2>Blog Text</h2>
+            <h2 className="blogBody">Blog Text</h2>
+          </div>
+        </div>
+        <div className="blogPost">
+          <div className="postImage">
+            <img src="https://ih1.redbubble.net/image.1105996056.5179/flat,750x1000,075,f.jpg" />
+          </div>
+          <div className="blogText">
+            <h1 className="blogTitle">Blog Title</h1>
+            <h2 className="blogBody">Blog Text</h2>
           </div>
         </div>
       </div>
