dbbaa5ed854144df7e2aec0cbdf361593297d6ed
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-08 12:39:39 +1000

    Added BlogPost

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -2,19 +2,21 @@ import React, { Component } from "react";
 import NavbarTop from "./NavbarTop";
 import "../App.css";
 import Footer from "./Footer";
+import BlogPost from "./BlogPost";
 
-class Blog extends Component{
-    render(){
-        return(
-            <div>
-                <NavbarTop />
-                <br />
-                <br />
-                <br />
-                <h5>This is the blog.</h5>
-            </div>
-        );
-    }
+class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <NavbarTop />
+        <br />
+        <br />
+        <br />
+        <h5>This is the blog.</h5>
+        <BlogPost />
+      </div>
+    );
+  }
 }
 
 export default Blog;
---
+++ client/src/components/BlogPost.js
@@ -0,0 +1,14 @@
+import React, { Component, useState } from "react";
+import "../App.css";
+
+class BlogPost extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from BlogPost</h1>
+      </div>
+    );
+  }
+}
+
+export default BlogPost;
