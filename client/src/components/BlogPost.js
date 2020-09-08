3120d624eb26bc7341c047358618cb42b474fc82
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-08 23:07:59 +1000

    Added cardtitle and cardtext

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -21,7 +21,8 @@ class BlogPost extends Component {
             src="https://petapixel.com/assets/uploads/2019/11/Geert-Weggen_squirrel-wishes_00003677.jpg"
             alt="Card image cap"
           />
-          <CardFooter className="blogText">Architecture</CardFooter>
+          <CardTitle className="blogTitle">Blog Title</CardTitle>
+          <CardText className="blogTitle">Blog Text</CardText>
         </Card>
 
         <Card body>
@@ -30,7 +31,8 @@ class BlogPost extends Component {
             src="https://filmdaily.co/wp-content/uploads/2020/04/funny-animals-lede.jpg"
             alt="Card image cap"
           />
-          <CardFooter className="blogText">Photography</CardFooter>
+          <CardTitle className="blogTitle">Blog Title</CardTitle>
+          <CardText className="blogTitle">Blog Text</CardText>
         </Card>
 
         <Card body>
@@ -39,7 +41,8 @@ class BlogPost extends Component {
             src="https://www.nikon.com.au/tmp/Asia/2419865273/3760176746/2586568015/286546384/152549275/210978916/542011102/2974987658/1834978357/200150890/2821987493.jpg"
             alt="Card image cap"
           />
-          <CardFooter className="blogText">Art Product</CardFooter>
+          <CardTitle className="blogTitle">Blog Title</CardTitle>
+          <CardText className="blogTitle">Blog Text</CardText>
         </Card>
 
         <Card body>
@@ -48,7 +51,8 @@ class BlogPost extends Component {
             src="https://ih1.redbubble.net/image.1105996056.5179/flat,750x1000,075,f.jpg"
             alt="Card image cap"
           />
-          <CardFooter className="blogText">Painting</CardFooter>
+          <CardTitle className="blogTitle">Blog Title</CardTitle>
+          <CardText className="blogTitle">Blog Text</CardText>
         </Card>
       </div>
     );
