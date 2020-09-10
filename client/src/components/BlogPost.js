4462093fa9518d4995808adc56f2608a73fa7c65
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-10 15:44:24 +1000

    Changed the BlogPost from function to class

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -95,7 +95,7 @@
 
 // export default BlogPost;
 
-import React from "react";
+import React, { Component, useState } from "react";
 import {
   Card,
   Button,
@@ -106,74 +106,77 @@ import {
   CardSubtitle,
   CardBody,
 } from "reactstrap";
-const Example = (props) => {
-  return (
-    <div className="posts">
-      <CardGroup className="individualPost">
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://petapixel.com/assets/uploads/2019/11/Geert-Weggen_squirrel-wishes_00003677.jpg"
-            alt="Card image cap"
-          />
-        </Card>
-        <Card>
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-      </CardGroup>
-      <CardGroup className="individualPost">
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://filmdaily.co/wp-content/uploads/2020/04/funny-animals-lede.jpg"
-            alt="Card image cap"
-          />
-        </Card>
-        <Card>
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-      </CardGroup>
-      <CardGroup className="individualPost">
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src="https://www.nikon.com.au/tmp/Asia/2419865273/3760176746/2586568015/286546384/152549275/210978916/542011102/2974987658/1834978357/200150890/2821987493.jpg"
-            alt="Card image cap"
-          />
-        </Card>
-        <Card>
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-      </CardGroup>
-    </div>
-  );
-};
 
-export default Example;
+class BlogPost extends Component {
+  render() {
+    return (
+      <div className="posts">
+        <CardGroup className="individualPost">
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="https://petapixel.com/assets/uploads/2019/11/Geert-Weggen_squirrel-wishes_00003677.jpg"
+              alt="Card image cap"
+            />
+          </Card>
+          <Card>
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                This card has supporting text below as a natural lead-in to
+                additional content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </CardGroup>
+        <CardGroup className="individualPost">
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="https://filmdaily.co/wp-content/uploads/2020/04/funny-animals-lede.jpg"
+              alt="Card image cap"
+            />
+          </Card>
+          <Card>
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                This card has supporting text below as a natural lead-in to
+                additional content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </CardGroup>
+        <CardGroup className="individualPost">
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src="https://www.nikon.com.au/tmp/Asia/2419865273/3760176746/2586568015/286546384/152549275/210978916/542011102/2974987658/1834978357/200150890/2821987493.jpg"
+              alt="Card image cap"
+            />
+          </Card>
+          <Card>
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardSubtitle>Card subtitle</CardSubtitle>
+              <CardText>
+                This card has supporting text below as a natural lead-in to
+                additional content.
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </CardGroup>
+      </div>
+    );
+  }
+}
+
+export default BlogPost;
