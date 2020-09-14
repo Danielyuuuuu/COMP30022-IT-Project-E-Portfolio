be06dbbdef5025a38216bf4cc401d7bd8dba6069
Adrian <chihout@student.unimelb.edu.au>
2020-09-15 03:19:52 +1000

    added a new gridfs and model for store item

--- server/models/db.js
+++ server/models/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const path = require('path');
-const crypto = require('crypto');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
+const path = require("path");
+const crypto = require("crypto");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
 
-MONGO_URL =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_TABLE}?${process.env.DB_OPTIONS}`;
+MONGO_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_TABLE}?${process.env.DB_OPTIONS}`;
 
 mongoose.connect(MONGO_URL || "mongodb://localhost/COMP30022", {
   useNewUrlParser: true,
@@ -31,21 +30,49 @@ const storage = new GridFsStorage({
         if (err) {
           return reject(err);
         }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const filename = buf.toString("hex") + path.extname(file.originalname);
         const fileInfo = {
           filename: filename,
-          bucketName: 'uploads'
+          bucketName: "uploads",
         };
         resolve(fileInfo);
       });
     });
-  }
+  },
 });
+
+// create storage engine
+const storeStorage = new GridFsStorage({
+  url: MONGO_URL,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        file.name = req.body.name;
+        const name = file.originalname;
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: file.name,
+          bucketName: "storeitems",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+
 const upload = multer({ storage });
+const uploadStore = multer({ storage: storeStorage });
 
 // Load schemas and map to Mongoose models
 require("./book");
+require("./item");
+require("./User");
 
 // exports the var
 exports.upload = upload;
+exports.uploadStore = uploadStore;
 exports.db = db;
+exports.mongo_url = MONGO_URL;
