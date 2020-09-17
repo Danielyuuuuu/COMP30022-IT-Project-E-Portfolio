6350222b04319a1705696148bdd06b21679cf846
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-17 16:29:28 +1000

    Adds login link to the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,18 @@ import React from "react";
 import "../App.css";
 
 const Footer = () => (
-  <div className="footer">
-    <p>This is our footer</p>
+  <div
+    className="footer"
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <p>This is our footer&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </p>
+    <p>
+      <a href="http://localhost:8000/user/register">Admin Login</a>
+    </p>
   </div>
 );
 
