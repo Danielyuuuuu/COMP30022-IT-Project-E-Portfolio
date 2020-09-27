192292dd3a5543eaaf7319c9529ade67026ab6fc
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-27 22:04:18 +1000

    Changed DropzoneArea.js class to function

--- client/src/admin/components/DropzoneArea.js
+++ client/src/admin/components/DropzoneArea.js
@@ -1,51 +1,39 @@
-import React, { Component } from 'react'
-import {DropzoneDialog} from 'material-ui-dropzone'
-import Button from '@material-ui/core/Button';
- 
-export default class DropzoneDialogExample extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-            files: []
-        };
-    }
- 
-    handleClose() {
-        this.setState({
-            open: false
-        });
-    }
- 
-    handleSave(files) {
-        //Saving files to state for further use and closing Modal.
-        this.setState({
-            files: files,
-            open: false
-        });
-    }
- 
-    handleOpen() {
-        this.setState({
-            open: true,
-        });
-    }
- 
-    render() {
-        return (
-            <div>
-                <Button onClick={this.handleOpen.bind(this)}>
-                  Add Image
-                </Button>
-                <DropzoneDialog
-                    open={this.state.open}
-                    onSave={this.handleSave.bind(this)}
-                    acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
-                    showPreviews={true}
-                    maxFileSize={5000000}
-                    onClose={this.handleClose.bind(this)}
-                />
-            </div>
-        );
-    }
+import React, { Component, useState } from "react";
+import { DropzoneDialog } from "material-ui-dropzone";
+import Button from "@material-ui/core/Button";
+
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
+
+export default function DropZone() {
+  const [open, setOpen] = useState(false);
+  const [files, setFiles] = useState([]);
+
+  const handleClose = async () => {
+    setOpen(false);
+  };
+
+  const handleSave = async (files) => {
+    //Saving files to state for further use and closing Modal.
+    setFiles(files);
+    setOpen(false);
+  };
+
+  const handleOpen = async () => {
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <Button onClick={() => handleOpen()}>Add Image</Button>
+      <DropzoneDialog
+        open={open}
+        onSave={(e) => handleSave()}
+        acceptedFiles={["image/jpeg", "image/png", "image/bmp"]}
+        showPreviews={true}
+        maxFileSize={5000000}
+        onClose={() => handleClose()}
+      />
+    </div>
+  );
 }
