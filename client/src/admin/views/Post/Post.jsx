326adecdfe6f168852c2c60ef6ecfcc8ccd9a816
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-27 18:04:41 +1000

    Linked Blog front end upload to back end API

--- client/src/admin/views/Post/Post.jsx
+++ client/src/admin/views/Post/Post.jsx
@@ -1,84 +1,113 @@
-import React from 'react';
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 // import { makeStyles } from '@material-ui/core/styles';
 
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import PostList from "../../components/PostList/PostList"
-
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+// import Button from "@material-ui/core/Button";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import PostList from "../../components/PostList/PostList";
+import { Button } from "reactstrap";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
 
 let marked = require("marked");
 
-const options = ['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4'];
+const options = ["Tag 1", "Tag 2", "Tag 3", "Tag 4"];
 
-export default function Post(){
+export default function Post() {
+  const [markdown, setMarkDown] = useState("");
+  const [tag, setTags] = React.useState([]);
+  const [title, setTitle] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
+  const history = useHistory();
 
-    const [markdown, setMarkDown] = useState("");
-    const [tag, setTags] = React.useState([]);
+  const submit = async (e) => {
+    e.preventDefault();
 
-    return (
-            <div>
-                
-                <PostList/>
-                
-                <h3>Edit the Post</h3>
-                <div>
-                    <TextField required id="standard-required1" label="Post Title"/>
-                </div>
-                
-                
-                <div>
-                    <TextField required id="standard-require2" label="Image Url" />
-                </div>
-                <Autocomplete
-                    multiple
-                    value={tag}
-                    onChange={(event, newValue) => {
-                    setTags(newValue);
-                    }}
-                    id="controllable-states-demo"
-                    options={options}
-                    
-                    defaultValue={[options[0]]}
-                    renderInput={(params) => (
-                        <TextField {...params} 
-                        label="Controllable" 
-                        variant="outlined" />)
-                    }
-                />
-                <Grid 
-                    
-                    container spacing={2} 
-                    direction="row" 
-                    justify="space-around" 
-                    alignItems="stretch">
+    const newPost = {
+      postTitle: title,
+      imageUrl: imageUrl,
+      postBody: markdown,
+      hashTags: tag,
+    };
+    const postRes = await Axios.post(
+      "http://localhost:8000/api/blog/uploadblog",
+      newPost
+    );
+    console.log("successful................");
+    history.push("/admin/dashboard");
+  };
 
-                    <Grid item xs={6}>
-                        <TextField
-                            id="filled-multiline-static"
-                            label="Post Content"
-                            multiline
-                            fullWidth
-                            rows={40}
-                            defaultValue="Default Value"
-                            variant="filled"
-                            value = {markdown}
-                            onChange = {(event)=>setMarkDown(event.target.value)}
-                        />
-                    </Grid>
+  return (
+    <div>
+      <PostList />
 
-                    <Grid item xs={6}>
-                        <div dangerouslySetInnerHTML = {{__html: marked(markdown)}}>
-                        </div>
-                    </Grid>
-                </Grid>
-                <Button variant="contained" color="primary" disableElevation>
-                    Submit!
-                </Button>
-            </div>
-    )
+      <h3>New Post</h3>
+      <div>
+        <TextField
+          id="Post Title"
+          name="Post Title"
+          label="Post Title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <TextField
+          id="Image Url"
+          name="Image Url"
+          label="Image Url"
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+      </div>
+      <Autocomplete
+        multiple
+        value={tag}
+        onChange={(event, newValue) => {
+          setTags(newValue);
+        }}
+        id="controllable-states-demo"
+        options={options}
+        defaultValue={[options[0]]}
+        renderInput={(params) => (
+          <TextField {...params} label="Controllable" variant="outlined" />
+        )}
+      />
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="space-around"
+        alignItems="stretch"
+      >
+        <Grid item xs={6}>
+          <TextField
+            id="filled-multiline-static"
+            label="Post Content"
+            multiline
+            fullWidth
+            rows={40}
+            defaultValue="Default Value"
+            variant="filled"
+            value={markdown}
+            onChange={(event) => setMarkDown(event.target.value)}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <div dangerouslySetInnerHTML={{ __html: marked(markdown) }}></div>
+        </Grid>
+      </Grid>
+      <Button
+        variant="contained"
+        color="primary"
+        disableElevation
+        onClick={submit}
+      >
+        Submit!
+      </Button>
+    </div>
+  );
 }
-
