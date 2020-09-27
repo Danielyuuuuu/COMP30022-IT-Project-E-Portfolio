074b0304fb56756a15a9428cb1f709c97131c4a7
Zardo <18664433198@163.com>
2020-09-27 20:07:50 +1000

    fix with upload

--- server/server.js
+++ server/server.js
@@ -12,13 +12,11 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const cookieParser = require("cookie-parser");
 const paypal = require('paypal-rest-sdk'); // for the paypal sdk
+const expressWs = require('express-ws'); //for the comment 
+
 
 // Start express
 const app = express();
-
-//
-const http = require("http").Server(app); // ?
-const io = require("socket.io")(http); // use socket to implement real-time comments
 
 // Connect Database
 require("./models/db");
@@ -61,7 +59,7 @@ app.use(bodyParser.json());
 app.use(methodOverride("_method"));
 
 // Setup routes
-app.use("/api", router);
+app.use("/", router);
 
 // server static assets if in production
 const build = path.join(__dirname, "../client/build/");
@@ -73,47 +71,22 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-//temp area
-//////////////////////////////////
-/////////////////////////////////
-///////////////////////////////
-/////////////////////////////
-var Posts = require('./models/posts');
-var Comments = require('./models/comments');
+//set the env of the comment 
 
-app.use(express.static(__dirname + "/public" ));
-/*
-app.get('/',function(req,res){
-  Posts.find({}, function(err, posts) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('commentsPage', { posts: posts });
-      }
-  }); 
-});
-*/
+const wsInstance = expressWs(app);
 
-app.get('/posts/detail/:id',function(req,res){
-  Posts.findById(req.params.id, function (err, postDetail) {
-      if (err) {
-        console.log(err);
-      } else {
-          Comments.find({'postId':req.params.id}, function (err, comments) {
-              res.render('postsPage', { postDetail: postDetail, comments: comments, postId: req.params.id });
-          });
-      }
-  }); 
-});
+app.ws('/comment', (ws, req) => {
 
-
-io.on('connection',function(socket){
-  socket.on('comment',function(data){
-      var commentData = new Comments(data);
-      commentData.save();
-      socket.broadcast.emit('comment',data);  
+  ws.on('message', function incoming(message) {
+    console.log(message) ;
+    ws.broadcast(message);
   });
 
+  ws.broadcast = function broadcast(data) {
+    wsInstance.getWss().clients.forEach(function each(client) {
+    client.send(data);
+    });
+  };
 });
 
 
