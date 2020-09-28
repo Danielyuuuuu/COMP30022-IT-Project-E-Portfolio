ea823023d879381b53f09ee6a59f3e2f65d42561
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-09-29 00:48:41 +1000

    add images Array for Stores

--- client/src/components/Store.js
+++ client/src/components/Store.js
@@ -9,6 +9,7 @@ import "../App.css";
 
 import { Card, Button, CardTitle, CardText, CardImg, Row, Col } from 'reactstrap';
 
+/*
 const StoreItems = (props) => {
   return (
     <div>
@@ -43,34 +44,88 @@ const StoreItems = (props) => {
 
     </div> 
 
-        
-
   );
 };
+*/
 
+const data = [{
+	id: 2,
+	name: "Forest",
+	image: "https://images.unsplash.com/photo-1468851508491-4f854ec88aa0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=b1222b6a1d3694cac76d2a23c3a02254"
+}, {
+	id: 4,
+	name: "Mountain",
+	image: "https://images.unsplash.com/photo-1467890947394-8171244e5410?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=9396f0adf263b51b44626228225684d0"
+}, {
+	id: 5,
+	name: "Boat",
+	image: "https://images.unsplash.com/photo-1443302382600-0bfacc473376?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=0c0f26518c1001f67b6c2e4480a8d3e0"
+}, {
+	id: 6,
+	name: "Flowers",
+	image: "https://images.unsplash.com/photo-1429091443922-e7d9ae79a837?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=e81cb6a60c53788559edb9bec21b80fc"
+}
 
-
+];
 
 class Store extends Component{
-
-
-    render(){
-        return(
-            <div>
-                <NavbarTop />
-                
-                <div className="store">
-                  <div className="store-menu">
-                    <StoreCategoryList />
-                  </div>
-                  <div className="store-items">
-                    <StoreItems />
-                  </div>
+  render(){
+      return(
+          <div>
+              <NavbarTop />
+              
+              <div className="store">
+                <div className="store-menu">
+                  <StoreCategoryList />
                 </div>
-                <Footer />
-            </div>
-        );
-    }
+                <div className="store-items">
+                  <StoreItems data={data}/>
+                </div>
+              </div>
+              <Footer />
+          </div>
+      );
+  }
 }
+
+class StoreItems extends React.Component {
+	render() {
+		return (
+            <div>
+			    <Items data={this.props.data} />
+            </div>
+		);
+	}
+}
+
+class Items extends Component{
+  render(){
+    return(
+      <div className="store-items-flexible">
+				{this.props.data.map((data) => {
+					return <Item data={data} key={data.id} />
+				})}
+			</div>
+    );
+  }
+}
+
+class Item extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <Card body className="store-itemCard">
+        <CardImg src={this.props.data.image} alt={this.props.data.name} />
+          <CardTitle>Photography  $3.00</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button>Buy now</Button>
+        </Card>
+    );
+  }
+}
+
+
 
 export default Store;
