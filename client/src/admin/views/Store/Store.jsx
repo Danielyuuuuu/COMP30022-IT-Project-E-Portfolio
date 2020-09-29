8f33449554f82e542bdbae37e01948a3a33c9844
dongchengd1 <48665115+rexding97@users.noreply.github.com>
2020-09-29 21:55:27 +1000

    complete the Store backpanel functions.

--- client/src/admin/views/Store/Store.jsx
+++ client/src/admin/views/Store/Store.jsx
@@ -1,66 +1,97 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import { Button, Container } from "@material-ui/core";
+import { useHistory } from 'react-router-dom';
+import Dialogs from "../../components/Dialogs/Dialogs";
 
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import { Button } from '@material-ui/core';
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import DeleteIcon from '@material-ui/icons/Delete';
 
-import Dialogs from "../../components/Dialogs/Dialogs"
-
+// import Joke from "../../components/Joke"
 const useRowStyles = makeStyles({
   root: {
-    '& > *': {
-      borderBottom: 'unset',
+    "& > *": {
+      borderBottom: "unset",
     },
+  },
+  img: {
+    width: 160,
+    height: 90,
   },
 });
 
-function createData(name, Stock, fat, Price, Options, price) {
-  return {
-    name,
-    Stock,
-    fat,
-    Price,
-    Options,
-    price,
-    details: { image: 'https://abc.def.com', description: "This item is really nice!" },
-  };
-}
-
 function Row(props) {
+  const history = useHistory();
   const { row } = props;
   const [open, setOpen] = React.useState(false);
   const classes = useRowStyles();
+
+  const handleDelete = (id) => {
+    axios.delete("http://localhost:8000/api/store/delete/"+id)
+        .then(console.log("delete item......"))
+        .then((res) => {
+          console.log(res);
+          history.go(0);
+        });
+    
+  };
 
   return (
     <React.Fragment>
       <TableRow className={classes.root}>
         <TableCell>
-          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-          {row.name}
+          <h4>{row.itemname}</h4>
         </TableCell>
-        <TableCell align="right">{row.Stock}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.Price}</TableCell>
+        <TableCell align="right">{row.tag}</TableCell>
+        <TableCell align="right">{row.stocks}</TableCell>
+        <TableCell align="right">{row.price}</TableCell>
+        <TableCell align="right">{row.views}</TableCell>
         <TableCell align="right">
-            {/* {row.Options}  */}
-            <Dialogs mode={"Edit"} variant="contained" color="primary"></Dialogs>
+        <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        startIcon={<DeleteIcon />}
+        onClick={(e)=>{handleDelete(row._id)}}
+      >
+        Delete
+      </Button>
+        </TableCell>
+        <TableCell align="right">
+          {/* {row.Options}  */}
+          <Dialogs
+            mode={"Edit"}
+            variant="contained"
+            color="primary"
+            item={row}
+          ></Dialogs>
         </TableCell>
       </TableRow>
       <TableRow>
@@ -75,19 +106,25 @@ function Row(props) {
                   <TableRow>
                     <TableCell>Image</TableCell>
                     <TableCell align="right">Description</TableCell>
-                    
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  
-                    <TableRow key={row.details.image}>
-                      <TableCell component="th" scope="row">
-                        {row.details.image}
-                      </TableCell>
-                      <TableCell align="right">{row.details.description}</TableCell>
-                    
-                    </TableRow>
-                  
+                  <TableRow key={row.imagename}>
+                    <TableCell component="th" scope="row">
+                      <Card className={classes.img}>
+                        <CardActionArea>
+                          <CardMedia
+                            className={classes.img}
+                            image={
+                              "http://localhost:8000/api/uploadManage/image/" +
+                              row.imagename
+                            }
+                          />
+                        </CardActionArea>
+                      </Card>
+                    </TableCell>
+                    <TableCell align="right">{row.description}</TableCell>
+                  </TableRow>
                 </TableBody>
               </Table>
             </Box>
@@ -98,60 +135,62 @@ function Row(props) {
   );
 }
 
-Row.propTypes = {
-  row: PropTypes.shape({
-    Stock: PropTypes.number.isRequired,
-    Price: PropTypes.number.isRequired,
-    fat: PropTypes.number.isRequired,
-    details: 
-      PropTypes.shape({
-        image: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-      }).isRequired,
-    name: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    Options: PropTypes.number.isRequired,
-  }).isRequired,
-};
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
-
 export default function Store() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // Read the mutable latest value
+    console.log(`Getting files...`);
+
+    axios
+      .get("http://localhost:8000/api/store/")
+      .then((res) => {
+        setItems(res.data.item);
+        console.log(res.data.item);
+      })
+      .catch((err) => {
+        console.log("Error from ShowBookList");
+      });
+  }, []);
+
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell><Dialogs mode={"New"} variant="contained" color="Secondary"></Dialogs></TableCell>
-            <TableCell>Item </TableCell>
-            <TableCell align="right">Stock</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Like</TableCell>
-            <TableCell align="right">Options</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <Row key={row.name} row={row} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Dialogs
+                  mode={"New"}
+                  variant="contained"
+                  color="Secondary"
+                  item={{
+                    itemname: "",
+                    stock: 0,
+                    price: 0,
+                    views: 0,
+                    imagename: "",
+                    description: "",
+                  }}
+                ></Dialogs>
+              </TableCell>
+              <TableCell>Item </TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Stock</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Views</TableCell>
+              <TableCell align="center" colSpan={2}>
+                Options
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((row) => (
+              <Row key={row.itemname} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
-
