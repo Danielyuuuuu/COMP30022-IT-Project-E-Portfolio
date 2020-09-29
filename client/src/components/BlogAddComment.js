3e675c4ecb755a1b4d7ddaa9f745997f550b9915
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-29 20:37:52 +1000

    The individual post page will automatically refresh after a new comment submission

--- client/src/components/BlogAddComment.js
+++ client/src/components/BlogAddComment.js
@@ -19,84 +19,80 @@ import {
   Media,
 } from "reactstrap";
 import { Comment, Icon, Header } from "semantic-ui-react";
-import axios from 'axios';
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 export default class addComment extends Component {
-    constructor(props){
-      super(props);
-        
-      this.writingComment = this.writingComment.bind(this);
-      this.writingPublisher = this.writingPublisher.bind(this);
-      this.submitComment = this.submitComment.bind(this);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        content: "",
-        publisher: "",
-        blogId :"5f703c3b07005138f07a2107"
-      }
-    }
-  
-    writingPublisher(e){
-      this.setState({
-        publisher : e.target.value
-      });
-    }
-    writingComment(e){
-      this.setState({
-        content : e.target.value
-      })
-    }
-  
-    submitComment(e){
-        e.preventDefault();
-        const comment = {
-        content : this.state.content,
-        publisher: this.state.publisher,
-        blogId : this.state.blogId
-      }
-      console.log("Submit success ")
-      console.log(e);
-      axios.post("http://localhost:8000/api/comments/add", comment)
-      .then(res =>{
-        
-        this.setState({
-          content : "",
-          publisher : ""
-        })
-      })
-    }
-  
-    render(){
-      return (
-        <div>
-          <Form style={{ marginTop: 20 }}>
-              <FormGroup>
-                <Input
-                  required
-                  type="text"
-                  value={this.state.publisher}
-                  placeholder="Enter your name..."
-                  onChange={this.writingPublisher}
+    this.writingComment = this.writingComment.bind(this);
+    this.writingPublisher = this.writingPublisher.bind(this);
+    this.submitComment = this.submitComment.bind(this);
 
-                />
-              </FormGroup>
-              <FormGroup>
-                <Input
-                  required
-                  type="textarea"
-                  value ={this.state.comment}
-                  placeholder="Leave a Comment..."
-                  onChange={this.writingComment}
-                />
-              </FormGroup>
-              <Button size="sm" onClick={this.submitComment}>Submit Comment</Button>
-            </Form>
-        </div>
-      );
-    }
-  
-      
-    
-  
-  
+    this.state = {
+      content: "",
+      publisher: "",
+      blogId: "5f71c425ec797250a88b4701",
+    };
   }
+
+  writingPublisher(e) {
+    this.setState({
+      publisher: e.target.value,
+    });
+  }
+  writingComment(e) {
+    this.setState({
+      content: e.target.value,
+    });
+  }
+
+  submitComment(e) {
+    e.preventDefault();
+    const comment = {
+      content: this.state.content,
+      publisher: this.state.publisher,
+      blogId: this.state.blogId,
+    };
+    console.log("Submit success ");
+    console.log(e);
+    axios
+      .post("http://localhost:8000/api/comments/add", comment)
+      .then((res) => {
+        this.setState({
+          content: "",
+          publisher: "",
+        });
+        window.location.reload(false);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Form style={{ marginTop: 20 }}>
+          <FormGroup>
+            <Input
+              required
+              type="text"
+              placeholder="Enter your name..."
+              onChange={this.writingPublisher}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Input
+              required
+              type="textarea"
+              placeholder="Leave a Comment..."
+              onChange={this.writingComment}
+            />
+          </FormGroup>
+          <Button size="sm" onClick={this.submitComment}>
+            Submit Comment
+          </Button>
+        </Form>
+      </div>
+    );
+  }
+}
--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,70 +1,68 @@
-const Comments = require('../models/comments');
+const Comments = require("../models/comments");
 
-
-const getComments = async(req,res) => {
-    Comments.find()
-        .populate('blogId')
-        .sort({'createdAt':-1})
-        .then(comments => res.json(comments))
-        .catch(err => res.status(400).json('Error' + err));
+const getComments = async (req, res) => {
+  Comments.find()
+    .populate("blogId")
+    .sort({ createdAt: -1 })
+    .then((comments) => res.json(comments))
+    .catch((err) => res.status(400).json("Error" + err));
 };
 
-
-const getBlogComments = async(req,res) =>{
-    console.log(req.params.id);
-    await Comments
-    .find({blog: req.params.id})
-    .sort({'createdAt':-1})
-    .then(comments => res.status(200).json({
+const getBlogComments = async (req, res) => {
+  console.log(req.params.id);
+  await Comments.find({ blog: req.params.id })
+    .sort({ createdAt: -1 })
+    .then((comments) =>
+      res.status(200).json({
         success: true,
         item: comments,
-    }))
-    .catch(err => res.status(400).json('Error' + err));
+      })
+    )
+    .catch((err) => res.status(400).json("Error" + err));
 };
 
-const getSingleComment = async(req,res)=>{
-    console.log(req.params.id);
-    Comments.findById(req.params.id)    
-    .then(comment => res.json(comment))
-    .catch(err => res.status(404).json({ nobookfound: "No comment found" }))
+const getSingleComment = async (req, res) => {
+  console.log(req.params.id);
+  Comments.findById(req.params.id)
+    .then((comment) => res.json(comment))
+    .catch((err) => res.status(404).json({ nobookfound: "No comment found" }));
 };
 
-const addComment = async (req,res) => {
-
-    //console.log(req.body);
-    const comment = new Comments({
-        blog : req.body.blogId,
-        publisher: req.body.publisher,
-        content : req.body.content,
-    });
-    console.log(comment);
-    try {
-        const savedComment = await comment.save()
-        .then((comm) => {
-            res.status(200).json({
-              success: true,
-              comm,
-            });
-          })
-          .catch((err) => res.status(400).json(err));
-    }catch(err){
-        res.status(400).send(err);
-    }
+const addComment = async (req, res) => {
+  //console.log(req.body);
+  const comment = new Comments({
+    blog: req.body.blogId,
+    publisher: req.body.publisher,
+    content: req.body.content,
+  });
+  console.log(comment);
+  try {
+    const savedComment = await comment
+      .save()
+      .then((comm) => {
+        res.status(200).json({
+          success: true,
+          comm,
+        });
+      })
+      .catch((err) => res.status(400).json(err));
+  } catch (err) {
+    res.status(400).send(err);
+  }
 };
 
-const deleteComment = async(req,res) => {
-    Comments.findByIdAndDelete({_id: req.params.id})
-    .then(comment => res.json({mgs:"comment have been deleted successfully"}))
-    .catch(err => res.status(404).json({error :"no such comment"}))
-}
-
-
-
+const deleteComment = async (req, res) => {
+  Comments.findByIdAndDelete({ _id: req.params.id })
+    .then((comment) =>
+      res.json({ mgs: "comment have been deleted successfully" })
+    )
+    .catch((err) => res.status(404).json({ error: "no such comment" }));
+};
 
 module.exports = {
-    getComments,
-    getBlogComments,
-    getSingleComment,
-    addComment,
-    deleteComment,
-}
+  getComments,
+  getBlogComments,
+  getSingleComment,
+  addComment,
+  deleteComment,
+};
