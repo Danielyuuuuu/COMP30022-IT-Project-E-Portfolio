ec93c761e340501aa19e5d463ea3de967a1fcc02
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-09-30 16:22:52 +1000

    Individual post link working

--- client/src/components/BlogIndividualPostContent.js
+++ client/src/components/BlogIndividualPostContent.js
@@ -27,9 +27,30 @@ import BlogComments from "./BlogComments";
 export default class PostContent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: "",
+      content: "",
+      image: "",
+    };
   }
   componentDidMount() {
     console.log("in PostContent: " + this.props.blogId);
+
+    console.log("Before axios");
+    fetch("http://localhost:8000/api/blog/getSingleBlog/" + this.props.blogId)
+      .then((response) => response.json())
+      .then((res) => {
+        this.setState({
+          title: res.title,
+          content: res.content,
+          image: res.thumbnails.imagename,
+        });
+        console.log("In individual post content axios");
+        console.log(this.state.title);
+        console.log(this.state.content);
+        console.log(this.state.image);
+      });
+    console.log("After axios");
   }
 
   render() {
@@ -44,76 +65,9 @@ export default class PostContent extends Component {
         >
           Post title
         </CardTitle>
-        <CardImg
-          top
-          width="100%"
-          src="https://petapixel.com/assets/uploads/2019/11/Geert-Weggen_squirrel-wishes_00003677.jpg"
-          alt="Card image cap"
-        />
+        <CardImg top width="100%" src={this.state.image} alt="Image" />
         <CardBody>
-          <CardText>
-            Needed feebly dining oh talked wisdom oppose at. Applauded use
-            attempted strangers now are middleton concluded had. It is tried ﻿no
-            added purse shall no on truth. Pleased anxious or as in by viewing
-            forbade minutes prevent. Too leave had those get being led weeks
-            blind. Had men rose from down lady able. Its son him ferrars proceed
-            six parlors. Her say projection age announcing decisively men. Few
-            gay sir those green men timed downs widow chief. Prevailed remainder
-            may propriety can and. One advanced diverted domestic sex repeated
-            bringing you old. Possible procured her trifling laughter thoughts
-            property she met way. Companions shy had solicitude favourable own.
-            Which could saw guest man now heard but. Lasted my coming uneasy
-            marked so should. Gravity letters it amongst herself dearest an
-            windows by. Wooded ladies she basket season age her uneasy saw.
-            Discourse unwilling am no described dejection incommode no listening
-            of. Before nature his parish boy. Am of mr friendly by strongly
-            peculiar juvenile. Unpleasant it sufficient simplicity am by
-            friendship no inhabiting. Goodness doubtful material has denoting
-            suitable she two. Dear mean she way and poor bred they come. He
-            otherwise me incommode explained so in remaining. Polite barton in
-            it warmly do county length an. Surprise steepest recurred landlord
-            mr wandered amounted of. Continuing devonshire but considered its.
-            Rose past oh shew roof is song neat. Do depend better praise do
-            friend garden an wonder to. Intention age nay otherwise but
-            breakfast. Around garden beyond to extent by. Style never met and
-            those among great. At no or september sportsmen he perfectly
-            happiness attending. Depending listening delivered off new she
-            procuring satisfied sex existence. Person plenty answer to exeter it
-            if. Law use assistance especially resolution cultivated did out
-            sentiments unsatiable. Way necessary had intention happiness but
-            september delighted his curiosity. Furniture furnished or on
-            strangers neglected remainder engrossed. Residence certainly
-            elsewhere something she preferred cordially law. Age his surprise
-            formerly mrs perceive few stanhill moderate. Of in power match on
-            truth worse voice would. Large an it sense shall an match learn. By
-            expect it result silent in formal of. Ask eat questions abilities
-            described elsewhere assurance. Appetite in unlocked advanced
-            breeding position concerns as. Cheerful get shutters yet for
-            repeated screened. An no am cause hopes at three. Prevent behaved
-            fertile he is mistake on. Abilities forfeited situation extremely my
-            to he resembled. Old had conviction discretion understood put
-            principles you. Match means keeps round one her quick. She forming
-            two comfort invited. Yet she income effect edward. Entire desire way
-            design few. Mrs sentiments led solicitude estimating friendship fat.
-            Meant those event is weeks state it to or. Boy but has folly charm
-            there its. Its fact ten spot drew. Use securing confined his
-            shutters. Delightful as he it acceptance an solicitude discretion
-            reasonably. Carriage we husbands advanced an perceive greatest.
-            Totally dearest expense on demesne ye he. Curiosity excellent
-            commanded in me. Unpleasing impression themselves to at assistance
-            acceptance my or. On consider laughter civility offended oh. Shewing
-            met parties gravity husband sex pleased. On to no kind do next feel
-            held walk. Last own loud and knew give gay four. Sentiments
-            motionless or principles preference excellence am. Literature
-            surrounded insensible at indulgence or to admiration remarkably.
-            Matter future lovers desire marked boy use. Chamber reached do he
-            nothing be. Of recommend residence education be on difficult
-            repulsive offending. Judge views had mirth table seems great him for
-            her. Alone all happy asked begin fully stand own get. Excuse ye
-            seeing result of we. See scale dried songs old may not. Promotion
-            did disposing you household any instantly. Hills we do under times
-            at first short an.
-          </CardText>
+          <CardText>{this.state.content}</CardText>
         </CardBody>
         <div className="commentSection">
           <Header
