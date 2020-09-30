b97d70d6b62233ec4915cbce991335cebb42af46
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-10-01 03:19:28 +1000

    add Modal for CheckOut button

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -27,6 +27,9 @@ import {
   Button,
 } from "reactstrap";
 
+import { useState } from "react";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
 const shoppingCartStyle = {
   position: "absolute",
   right: "30px",
@@ -71,15 +74,83 @@ class ShoppingCart extends React.Component {
           </FormGroup>
 
           <DropdownItem divider />
-          <DropdownItem>
-            <Button outline color="success">
+          {/* <DropdownItem> */}
+          {/* <Button outline color="success">
               Check Out
-            </Button>
-          </DropdownItem>
+            </Button> */}
+          <CheckOutModal buttonLabel="Check Out" className="" />
+          {/* </DropdownItem> */}
         </DropdownMenu>
       </UncontrolledDropdown>
     );
   }
 }
+
+const CheckOutModal = (props) => {
+  const { buttonLabel, className } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  const closeBtn = (
+    <button className="close" onClick={toggle}>
+      &times;
+    </button>
+  );
+
+  return (
+    <div>
+      <Button color="success" onClick={toggle}>
+        {buttonLabel}
+      </Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle} close={closeBtn}>
+          Check Out List:
+        </ModalHeader>
+        <ModalBody>
+          <FormGroup style={dropDownButtonStyle} color="success">
+            <div>
+              <CustomInput
+                type="checkbox"
+                id="exampleCustomCheckbox"
+                label="painting1"
+              />
+              <br />
+              <CustomInput
+                type="checkbox"
+                id="exampleCustomCheckbox2"
+                label="photography2"
+              />
+              <br />
+              <CustomInput
+                type="checkbox"
+                id="exampleCustomCheckbox3"
+                label="art product3"
+              />
+              <br />
+              <CustomInput
+                type="checkbox"
+                id="exampleCustomCheckbox4"
+                label="photography4"
+              />
+              <br />
+              <br />
+              In Total: $12.00
+            </div>
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Pay Now
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
 
 export default ShoppingCart;
