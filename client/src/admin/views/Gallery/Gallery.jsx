e3955126ee688a9538e07e273b65eea0a51f319b
dongchengd1 <48665115+rexding97@users.noreply.github.com>
2020-10-03 01:00:32 +1000

    prepare for connect gallery

--- client/src/admin/views/Gallery/Gallery.jsx
+++ client/src/admin/views/Gallery/Gallery.jsx
@@ -1,66 +1,262 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import IconButton from '@material-ui/core/IconButton';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import DeleteIcon from '@material-ui/icons/Delete';
+// import React from "react";
+// import MediaOptionBar from "../../components/MediaOptionBar/MediaOptionBar";
+// import {SelectedPictures} from "../../components/MediaOptionBar/SharedVar";
 
-const useStyles = makeStyles((theme) => ({
+// export default function Gallery() {
+//   const [selectedPictures, setPictures] = React.useState([]);
+//   return (
+//     <div>
+//       <SelectedPictures.Provider value={{ selectedPictures, setPictures }}>
+//         <MediaOptionBar></MediaOptionBar>
+//       </SelectedPictures.Provider>
+//       <div>
+//         {selectedPictures.map((picture) => (
+//           <div>{picture.filename}</div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import { Button, Container } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import Dialogs from "../../components/Dialogs/Dialogs";
+
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogActions from "@material-ui/core/DialogActions";
+
+// import Joke from "../../components/Joke"
+const useRowStyles = makeStyles({
   root: {
-    flexGrow: 1,
+    "& > *": {
+      borderBottom: "unset",
+    },
+  },
+  img: {
+    width: 160,
+    height: 90,
+  },
+});
 
-  },
-  demo: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  title: {
-    margin: theme.spacing(4, 0, 2),
-  },
-}));
+function Row(props) {
+  const history = useHistory();
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
 
-function generate(element) {
-  return [0, 1, 2, 4].map((value) =>
-    React.cloneElement(element, {
-      key: value,
-    }),
+  const [openDeleteAlert, setDeleteAlert] = React.useState(false);
+
+  const handleDelete = (id) => {
+    axios
+      .delete("http://localhost:8000/api/store/delete/" + id)
+      .then(console.log("delete item......"))
+      .then((res) => {
+        console.log(res);
+        history.go(0);
+      });
+    handleCloseDeleteAlert();
+  };
+  const handleClickDeleteAlert = () => {
+    setDeleteAlert(true);
+  };
+  const handleCloseDeleteAlert = () => {
+    setDeleteAlert(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Dialog
+        open={openDeleteAlert}
+        onClose={handleCloseDeleteAlert}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Are you trying to delete this item?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Once you agree, you cant make it back!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDeleteAlert} color="primary">
+            Disagree
+          </Button>
+          <Button
+            onClick={(e) => {
+              handleDelete(row._id);
+            }}
+            color="primary"
+            autoFocus
+          >
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <TableRow className={classes.root}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          <h4>{row.itemname}</h4>
+        </TableCell>
+        <TableCell align="right">{row.tag}</TableCell>
+        <TableCell align="right">{row.stocks}</TableCell>
+        <TableCell align="right">{row.price}</TableCell>
+        <TableCell align="right">{row.views}</TableCell>
+        <TableCell align="right">
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            startIcon={<DeleteIcon />}
+            onClick={handleClickDeleteAlert}
+          >
+            Delete
+          </Button>
+        </TableCell>
+        <TableCell align="right">
+          {/* {row.Options}  */}
+          <Dialogs
+            mode={"Edit"}
+            variant="contained"
+            color="primary"
+            item={row}
+          ></Dialogs>
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                Details
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Image</TableCell>
+                    <TableCell align="right">Description</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow key={row.imagename}>
+                    <TableCell component="th" scope="row">
+                      <Card className={classes.img}>
+                        <CardActionArea>
+                          <CardMedia
+                            className={classes.img}
+                            image={
+                              "http://localhost:8000/api/uploadManage/image/" +
+                              row.imagename
+                            }
+                          />
+                        </CardActionArea>
+                      </Card>
+                    </TableCell>
+                    <TableCell align="right">{row.description}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
   );
 }
 
-export default function Gallery() {
-  const classes = useStyles();
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
+export default function Store() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // Read the mutable latest value
+    console.log(`Getting files...`);
+
+    axios
+      .get("http://localhost:8000/api/store/")
+      .then((res) => {
+        setItems(res.data.item);
+        console.log(res.data.item);
+      })
+      .catch((err) => {
+        console.log("Error from ShowBookList");
+      });
+  }, []);
 
   return (
-    <div className={classes.root}>
-      
-        <Grid item xs={12} md={12}>
-          <Typography variant="h6" className={classes.title}>
-            Gallery List
-          </Typography>
-          <div className={classes.demo}>
-            <List dense={dense}>
-              {generate(
-                <ListItem>
-                  <ListItemText
-                    primary="Single-line item"
-                    secondary={secondary ? 'Secondary text' : null}
-                  />
-                  <ListItemSecondaryAction>
-                    <IconButton edge="end" aria-label="delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>,
-              )}
-            </List>
-          </div>
-        </Grid>
-
+    <div>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Dialogs
+                  mode={"New"}
+                  variant="contained"
+                  color="Secondary"
+                  item={{
+                    itemname: "",
+                    stock: 0,
+                    price: 0,
+                    views: 0,
+                    imagename: "",
+                    description: "",
+                  }}
+                ></Dialogs>
+              </TableCell>
+              <TableCell>Item </TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Stock</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Views</TableCell>
+              <TableCell align="center" colSpan={2}>
+                Options
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((row) => (
+              <Row key={row.itemname} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
--- client/src/admin/views/Media/Media.jsx
+++ client/src/admin/views/Media/Media.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import {useState,useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
-import Paper from '@material-ui/core/Paper';
-import Snackbar from '@material-ui/core/Snackbar';
-import Alert from '@material-ui/lab/Alert';
+import Paper from "@material-ui/core/Paper";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 // import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import HighlightOffOutlinedIcon from '@material-ui/icons/HighlightOffOutlined';
+import IconButton from "@material-ui/core/IconButton";
+import HighlightOffOutlinedIcon from "@material-ui/icons/HighlightOffOutlined";
 
-import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+import FileCopyOutlinedIcon from "@material-ui/icons/FileCopyOutlined";
 
-import DropzoneArea from '../../components/DropzoneArea';
+import DropzoneArea from "../../components/DropzoneArea";
 
-import axios from "axios"
-import { useHistory } from 'react-router-dom';
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,15 +27,14 @@ const useStyles = makeStyles((theme) => ({
   },
 
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
     backgroundColor: theme.palette.background.paper,
-    
   },
   icon: {
-      color: 'rgba(255, 255, 255, 0.54)'
+    color: "rgba(255, 255, 255, 0.54)",
   },
 
   control: {
@@ -43,18 +42,15 @@ const useStyles = makeStyles((theme) => ({
   },
 
   gridList: {
-    height: '800px',
-    width: '100%',
+    height: "800px",
+    width: "100%",
   },
 
   paper: {
-    height: '100%',
-    width: '100%',
-  }
-
+    height: "100%",
+    width: "100%",
+  },
 }));
-
-
 
 export default function SpacingGrid() {
   const [spacing, setSpacing] = React.useState(2);
@@ -67,85 +63,113 @@ export default function SpacingGrid() {
   const [copyOpen, setCopyOpen] = React.useState(false);
 
   const handleCopyClick = () => {
-        setCopyOpen(true);
-  }
+    setCopyOpen(true);
+  };
 
   const handleCopyClose = (event, reason) => {
-      if (reason === 'clickaway'){
-          return;
-      }
-      setCopyOpen(false);
-  }
+    if (reason === "clickaway") {
+      return;
+    }
+    setCopyOpen(false);
+  };
 
   const [images, setImages] = useState([]);
-  
 
-    useEffect(() => {
-          // Read the mutable latest value
-          console.log(`Getting files...`);
-        
-        axios
-        .get("http://localhost:8000/api/uploadManage/files")
-        .then((res) => {
-            setImages(res.data);
-        })
-        .catch((err) => {
-            console.log("Error from ShowBookList");
-        });
-      },[]);
+  useEffect(() => {
+    // Read the mutable latest value
+    console.log(`Getting files...`);
 
-//   const images = [
-//       {
-//           author: "abc"
-//       }
-//   ]
-    const history = useHistory();
+    axios
+      .get("http://localhost:8000/api/uploadManage/files")
+      .then((res) => {
+        setImages(res.data);
+      })
+      .catch((err) => {
+        console.log("Error from ShowBookList");
+      });
+  }, []);
+
+  //   const images = [
+  //       {
+  //           author: "abc"
+  //       }
+  //   ]
+  const history = useHistory();
   return (
     <div>
-        <Grid container spacing={2} direction="row" justify="space-around" alignItems="stretch">
-
-            <Grid item xs={8} className={classes.root}>
-                <Paper elevation={0} className={classes.paper}>
-                    <h3>Media</h3>
-                    <GridList cellHeight={180} className={classes.gridList} cols={3}>
-                        {images.map((tile) => (
-                        <GridListTile>
-                            <img src={"http://localhost:8000/api/uploadManage/image/"+tile.filename} />
-                            <GridListTileBar
-                                actionIcon={
-                                    <div>
-                                    <IconButton className={classes.icon} onClick={() => {navigator.clipboard.writeText("http://localhost:8000/api/uploadManage/image/"+tile.filename)}}>
-                                        <FileCopyOutlinedIcon onClick={handleCopyClick}/>
-                                    </IconButton>
-                                    <IconButton className={classes.icon} onClick={() => {axios.delete("http://localhost:8000/api/uploadManage/files/"+tile._id);history.go(0);}}>
-                                        <HighlightOffOutlinedIcon/>
-                                    </IconButton>
-                                    </div>
-                                }
-                                />
-                        </GridListTile>
-                        ))}
-                    </GridList>
-                </Paper>
-            </Grid>
-
-            <Grid item xs={4} className={classes.root}>
-                <Paper elevation={0} className={classes.paper}>
-                    <h3>Informations</h3>
-                    <p>File Name:</p>
-                    <p>Describe:</p>
-                    <p>URL:</p>
-                    <DropzoneArea />
-                </Paper>
-            </Grid>
-
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="space-around"
+        alignItems="stretch"
+      >
+        <Grid item xs={8} className={classes.root}>
+          <Paper elevation={0} className={classes.paper}>
+            <h3>Media</h3>
+            <GridList cellHeight={180} className={classes.gridList} cols={3}>
+              {images.map((tile) => (
+                <GridListTile>
+                  <img
+                    src={
+                      "http://localhost:8000/api/uploadManage/image/" +
+                      tile.filename
+                    }
+                  />
+                  <GridListTileBar
+                    actionIcon={
+                      <div>
+                        <IconButton
+                          className={classes.icon}
+                          onClick={() => {
+                            navigator.clipboard.writeText(
+                              "http://localhost:8000/api/uploadManage/image/" +
+                                tile.filename
+                            );
+                          }}
+                        >
+                          <FileCopyOutlinedIcon onClick={handleCopyClick} />
+                        </IconButton>
+                        <IconButton
+                          className={classes.icon}
+                          onClick={() => {
+                            axios.delete(
+                              "http://localhost:8000/api/uploadManage/files/" +
+                                tile._id
+                            );
+                            history.go(0);
+                          }}
+                        >
+                          <HighlightOffOutlinedIcon />
+                        </IconButton>
+                      </div>
+                    }
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </Paper>
         </Grid>
-        <Snackbar open={copyOpen} autoHideDuration={2000} onClose={handleCopyClose}>
+
+        <Grid item xs={4} className={classes.root}>
+          <Paper elevation={0} className={classes.paper}>
+            <h3>Informations</h3>
+            <p>File Name:</p>
+            <p>Describe:</p>
+            <p>URL:</p>
+            <DropzoneArea />
+          </Paper>
+        </Grid>
+      </Grid>
+      <Snackbar
+        open={copyOpen}
+        autoHideDuration={2000}
+        onClose={handleCopyClose}
+      >
         <Alert onClose={handleCopyClose} severity="success">
           You have already copy the url of image.
         </Alert>
-      </Snackbar>     
-                         
+      </Snackbar>
     </div>
   );
 }
