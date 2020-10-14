15bd94ce0679421c84c00dc2dcc060f63ecdfb37
AngelT99 <43331695+AngelT99@users.noreply.github.com>
2020-10-14 15:56:02 +1100

    remove two categories in portfolio, and modify the styling

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,83 +1,101 @@
 import React, { Component } from "react";
 import "../App.css";
 
-
-
-import { Card, Button, CardTitle, CardText, CardImg, Row, Col, CardFooter, CardLink } from 'reactstrap';
+import {
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  CardImg,
+  Row,
+  Col,
+  CardFooter,
+  CardLink,
+} from "reactstrap";
 
 const Example = (props) => {
   return (
     <div className="categories">
       <Card body>
         <CardLink href="/artworkgallery">
-          <CardImg width="100%" src="https://www.business.com/images/content/5ca/3d01e5a215e8a458b690f/0-800-" alt="Card image cap" />
+          <CardImg
+            width="100%"
+            src="https://www.business.com/images/content/5ca/3d01e5a215e8a458b690f/0-800-"
+            alt="Card image cap"
+          />
           <CardFooter className="text-center">Photography</CardFooter>
         </CardLink>
       </Card>
 
-
-      <Card body>
+      {/* <Card body>
         <CardLink href="/artworkgallery">
           <CardImg width="100%" src="https://static.dezeen.com/uploads/2019/12/logo-house-adidas-karina-wiciak_dezeen_2364_col_2.jpg" alt="Card image cap" />
           <CardFooter className="text-center">Architecture</CardFooter>
         </CardLink>
-      </Card>
-
-      
+      </Card> */}
 
       <Card body>
         <CardLink href="https://en.wikipedia.org/wiki/Art">
-          <CardImg width="100%" src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg" alt="Card image cap" />
-          <CardFooter className="text-center">Art Product</CardFooter>
+          <CardImg
+            width="100%"
+            src="https://kottke.org/plus/misc/images/art-institute-chi-01.jpg"
+            alt="Card image cap"
+          />
+          <CardFooter className="text-center">Art Crafts</CardFooter>
         </CardLink>
       </Card>
-      
+
       <Card body>
         <CardLink href="https://en.wikipedia.org/wiki/Painting">
-          <CardImg width="100%" src="https://wallpaperaccess.com/full/764272.jpg" alt="Card image cap" />
+          <CardImg
+            width="100%"
+            src="https://wallpaperaccess.com/full/764272.jpg"
+            alt="Card image cap"
+          />
           <CardFooter className="text-center">Painting</CardFooter>
         </CardLink>
       </Card>
 
-      <Card body>
+      {/* <Card body>
         <CardLink href="https://en.wikipedia.org/wiki/Film">
           <CardImg width="100%" src="https://sofy.tv/blog/wp-content/uploads/2020/01/filmmaking-production.jpg" alt="Card image cap" />
           <CardFooter className="text-center">Films</CardFooter>
         </CardLink> 
-      </Card>
+      </Card> */}
 
       <Card body>
         <CardLink href="https://en.wikipedia.org/wiki/Graphic">
-          <CardImg width="100%" src="https://www.thecreativeissue.com.au/wp-content/uploads/2017/07/Beastman-3-TCI.jpg" alt="Card image cap" />
+          <CardImg
+            width="100%"
+            src="https://www.thecreativeissue.com.au/wp-content/uploads/2017/07/Beastman-3-TCI.jpg"
+            alt="Card image cap"
+          />
           <CardFooter className="text-center">Graphic Design</CardFooter>
-        </CardLink>   
+        </CardLink>
       </Card>
-
-  </div>
-
+    </div>
   );
 };
 
 const divStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
-
-class Category extends Component{
-    render(){
-        return(
-            <div>
-                <br />
-                <br />
-                <br />
-                <div style={divStyle}>
-                    <Example />
-                </div>
-            </div>
-        );
-    }
+class Category extends Component {
+  render() {
+    return (
+      <div>
+        <br />
+        <br />
+        <br />
+        <div style={divStyle}>
+          <Example />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Category;
