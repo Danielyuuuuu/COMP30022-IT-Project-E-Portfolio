06688bb07f1b145eba2b71edb2417ab46d9648c6
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2020-10-17 13:21:16 +1100

    Changed the contactMe display style

--- client/src/admin/views/ContactMe/ContactMe.jsx
+++ client/src/admin/views/ContactMe/ContactMe.jsx
@@ -33,28 +33,28 @@ export default class ContactMe extends Component {
     this.setState({expanded: !this.state.expanded});
   }
 
-  classes = makeStyles((theme) => ({
-    root: {
-      maxWidth: 345,
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-  }));
+  // classes = makeStyles((theme) => ({
+  //   root: {
+  //     maxWidth: 200,
+  //   },
+  //   media: {
+  //     height: 0,
+  //     paddingTop: '56.25%', // 16:9
+  //   },
+  //   expand: {
+  //     transform: 'rotate(0deg)',
+  //     marginLeft: 'auto',
+  //     transition: theme.transitions.create('transform', {
+  //       duration: theme.transitions.duration.shortest,
+  //     }),
+  //   },
+  //   expandOpen: {
+  //     transform: 'rotate(180deg)',
+  //   },
+  //   avatar: {
+  //     backgroundColor: red[500],
+  //   },
+  // }));
 
   componentDidMount() {
     this.fetchAllTheContactMeMessages();
@@ -116,25 +116,26 @@ export default class ContactMe extends Component {
             );
           })}
         </Comment.Group>
-        <Card className={this.classes.root}>
+        <Card className="displayContactMe">
           <CardHeader
             avatar={
-              <Avatar aria-label="recipe" className={this.classes.avatar}>
+              <Avatar aria-label="recipe">
                 R
               </Avatar>
             }
             action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
+              <CardActions disableSpacing className="expandArrow">
+              <IconButton
+                onClick={this.handleExpandClick}
+                aria-expanded={this.state.expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
               </IconButton>
+              </CardActions>
             }
             title="Shrimp and Chorizo Paella"
             subheader="September 14, 2016"
-          />
-          <CardMedia
-            className={this.classes.media}
-            image="/static/images/cards/paella.jpg"
-            title="Paella dish"
           />
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
@@ -142,49 +143,9 @@ export default class ContactMe extends Component {
               guests. Add 1 cup of frozen peas along with the mussels, if you like.
             </Typography>
           </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>
-            <IconButton
-              className={clsx(this.classes.expand, {
-                [this.classes.expandOpen]: this.state.expanded,
-              })}
-              onClick={this.handleExpandClick}
-              aria-expanded={this.state.expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
           <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
             <CardContent>
-              <Typography paragraph>Method:</Typography>
-              <Typography paragraph>
-                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                minutes.
-              </Typography>
-              <Typography paragraph>
-                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-              </Typography>
-              <Typography paragraph>
-                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                minutes more. (Discard any mussels that don’t open.)
-              </Typography>
-              <Typography>
-                Set aside off of the heat to let rest for 10 minutes, and then serve.
-              </Typography>
+              <Typography paragraph>text!!!!!!!!!!!!!!!!!!</Typography>
             </CardContent>
           </Collapse>
         </Card>
