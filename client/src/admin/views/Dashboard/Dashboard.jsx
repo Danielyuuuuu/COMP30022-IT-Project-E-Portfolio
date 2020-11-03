d4c1249b114aac6e032522af487e572afa4580fd
dongchengd1 <48665115+rexding97@users.noreply.github.com>
2020-11-04 07:08:55 +1100

    add statistic chart

--- client/src/admin/views/Dashboard/Dashboard.jsx
+++ client/src/admin/views/Dashboard/Dashboard.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+import Chart from './Chart';
+
 export default function Dashboard() {
     const [statistic, setStatistic] = useState([]);
 
     const getStatistic = async () => {
-        console.log(`Getting Posts...`);
+        // console.log(`Getting Posts...`);
         axios
             .get("http://localhost:8000/api/statistic/")
             .then((res) => {
@@ -24,7 +34,29 @@ export default function Dashboard() {
 
     return (
         <div>
-            {statistic.map((day) => (
+            <div>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container justify="center" spacing={2}>
+                            {["mediaNumber", "blogNumber", "storeNumber", "messageNumber", "viewsNumber"].map((value) => (
+                                <Grid key={value} item>
+                                    <Card variant="outlined">
+                                        <CardContent>
+
+                                            <Typography variant="h5" component="h2">
+                                                {value}
+                                            </Typography>
+
+                                            <Chart data={statistic} y={value}></Chart>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
+            {/* {statistic.map((day) => (
                 <div>
                     <div>
                         {day.date}
@@ -40,12 +72,15 @@ export default function Dashboard() {
                     </div>
                     <div>
                         Message Number: {day.messageNumber}
-                    </div> 
+                    </div>
                     <div>
                         Views: {day.viewsNumber}
                     </div>
                 </div>
-            ))}
+            ))} */}
+
+
+
         </div>
     );
 }
